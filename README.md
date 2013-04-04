bd876eba5cca97c86f2bbd0fbe5e3f31b70e9837
piotrex <piotrekdn@gmail.com>
2013-04-04 21:34:19 +0200

    readme and icon --> url on github

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 CookiePopupBlocker
 ==================
 
-Blokuje banery z informacją o używaniu przez witrynę cookies
+Skrypt do Greasemonkey (*) blokujący wyskakujących okienek/banerów z informacjami o używaniu ciasteczek.
+
+[](https://docs.google.com/spreadsheet/ccc?key=0AgtalLhlHdWqdEljOTBWa2JhMmF2ei1ZZWxmVU5IZFE&usp=sharing)
+
+[Link do instalacji skryptu](https://github.com/piotrex/CookiePopupBlocker/raw/master/build/cookiepopupblocker-no_logs.user.js)
+
+(*) - jedynie na Greasemonkey były robione testy 
--- make.bat
+++ make.bat
@@ -1,14 +1,16 @@
 @echo off
 
 SET project_folder=%~dp0
-SET jsbeutify_folder=E:\Piotrek\Temp\jsbeutify
+REM SET jsbeutify_folder=E:\Piotrek\Temp\jsbeutify
 
 IF NOT EXIST "%project_folder%\build\" MD "%project_folder%\build" || pause
 IF NOT EXIST "%project_folder%\logs\" MD "%project_folder%\logs" || pause
 
 cd /D "E:\MinGW\bin\"
-path %jsbeutify_folder%;%PATH%
+REM path %jsbeutify_folder%;%PATH%
 
 SET input=cookiepopupblocker.user.js
-SET output=cookiepopupblocker.user.js
+REM SET output=cookiepopupblocker.user.js
+SET output=cookiepopupblocker-no_logs.user.js
+REM SET output=cookiepopupblocker-logs.user.js
 gcc -E -P -C -w -x c  "%project_folder%\%input%" -o "%project_folder%\build\%output%" || pause
